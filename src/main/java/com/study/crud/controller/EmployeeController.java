ff2fb356ce109a4db2e25bddb06463bebd826bd8
chaxiaoniu <1935003650@qq.com>
2018-03-07 17:20:58 +0800

    返回分页的json数据，返回值用通用的类来代替

--- src/main/java/com/study/crud/controller/EmployeeController.java
+++ src/main/java/com/study/crud/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.study.crud.bean.Employee;
 import com.study.crud.service.EmployeeService;
+import com.study.crud.utils.Msg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -58,15 +59,16 @@ public class EmployeeController {
 
     /**
      * 查询员工数据（分页查询）
-     * 返回值为 PageInfo，利用 @ResponseBody返回json数据，而不是页面，
-     *      访问地址：http://localhost:8080/ssm-crud/emps?pageNo=3 可以查看返回的 JSON字符串
+     * 返回值为 PageInfo，也可以设置通用的返回类 Msg
+     *  利用 @ResponseBody返回json数据，而不是页面，
+     *  访问地址：http://localhost:8080/ssm-crud/emps?pageNo=3 可以查看返回的 JSON字符串
      *
      * 注意：@ResponseBody正常工作需要 导入jackson包。负责将 对象 转换为 json字符串
      */
     @RequestMapping("/emps")
     @ResponseBody //返回json数据
-    public PageInfo getEmpsWithJson( @RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo,
-                           Model model) {
+    public Msg getEmpsWithJson(@RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo,
+                               Model model) {
         // 引入PageHelper分页插件
         // 在查询之前只需要调用 startPage，传入页码 pageNo，以及每页的大小 5
         PageHelper.startPage(pageNo, 5);
@@ -77,6 +79,6 @@ public class EmployeeController {
         // 使用pageInfo 包装查询后的结果，只需要将 pageInfo 交给页面就行了。
         // pageInfo 封装了详细的分页信息,包括有我们查询出来的数据。
         PageInfo page = new PageInfo(emps, 5);
-        return page;
+        return Msg.success().add("pageInfo",page);
     }
 }
