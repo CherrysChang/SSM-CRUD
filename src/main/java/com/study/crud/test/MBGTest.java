f1a27f3d7b30c4c2affda5bb9d89541a019abd2a
chaxiaoniu <1935003650@qq.com>
2018-03-07 11:41:22 +0800

    配置 MyBatis配置文件 以及利用逆向工程生成代码文件

---
+++ src/main/java/com/study/crud/test/MBGTest.java
@@ -0,0 +1,28 @@
+package com.study.crud.test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Desc:
+ * @author Qian
+ **/
+public class MBGTest {
+    //利用代码 运行 MyBatis 逆向工程 MBG，参考官网：http://www.mybatis.org/generator/running/running.html
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
